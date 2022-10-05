198e2c3ab450c13556c4c2e44f9ccc65de5c42bc
태민 <81548729+xoals6536@users.noreply.github.com>
2022-10-05 21:09:52 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.54 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int n=0;
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=left;i<=right;i++){
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    n += 1;
+                    
+                }
+            }
+            map.put(i,n);
+            n = 0;
+        }
+        System.out.println(map);
+        for (int i = left; i <= right ; i++) {
+            if(map.get(i)%2==0){
+                answer += i;
+            }else{
+                answer -= i;
+            }
+        }
+        return answer;
+    }
+}
