492174ed22623655b628d39037444cff67249f15
taemin <81548729+taemin6536@users.noreply.github.com>
2025-02-22 23:23:23 +0900

    Time: 6 ms (29.07%), Space: 44.5 MB (11.86%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = Integer.toString(x);
+        int len = str.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
