35d6587ae1cb5b548e5016f9881cba03958640b1
taemin <81548729+taemin6536@users.noreply.github.com>
2025-03-30 18:53:05 +0900

    Time: 12 ms (97.35%), Space: 46.6 MB (75.96%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 1; i++){
+            if(i % 2 == 0){
+                sum += nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
