62e115c572c81f0adf5f6d5f7cb44be9854a5695
taemin <81548729+taemin6536@users.noreply.github.com>
2025-04-07 17:31:19 +0900

    Time: 11 ms (19.96%), Space: 63.5 MB (62.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,19 +10,18 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> deque = new LinkedList<>();
         ListNode node = head;
 
         while(node != null){
-            stack.add(node.val);
+            deque.add(node.val);
             node = node.next;
         }
 
-        while(head != null){
-            if (head.val != stack.pop()) {
+        while(!deque.isEmpty() && deque.size() > 1){
+            if (deque.pollFirst() != deque.pollLast()){
                 return false;
             }
-            head = head.next;
         }
         return true;
     }
